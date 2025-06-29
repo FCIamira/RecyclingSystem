c56d05fd1de6cd33e8bd81f28e91d7ae22e553fa
madonna-hany-boils <mh2153@fayoum.edu.eg>
2025-06-29 14:22:22 +0300

    Add Rewards

---
+++ RecyclingSystem.Domain/Models/RewardRedemptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecyclingSystem.Domain.Common;
+using RecyclingSystem.Domain.Enums;
+namespace RecyclingSystem.Domain.Models
+{
+    internal class RewardRedemptions:BaseModel<int>
+    {
+        [ForeignKey("User")]
+       public int UserId {   get; set;}
+        [ForeignKey("Rewards")]
+       public int  RewardId {  get; set;}
+        public DateTime DateRedeemed {get;set;}
+        public Status Status { get; set;}
+
+        public virtual Rewards Rewards { get; set;}
+        //public virtual User User { get; set; }//not completed
+    }
+}
---
+++ RecyclingSystem.Domain/Models/Rewards.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecyclingSystem.Domain.Common;
+using static System.Net.Mime.MediaTypeNames;
+namespace RecyclingSystem.Domain.Models
+{
+    internal class Rewards:BaseModel<int>
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        [MaxLength(100, ErrorMessage = "Title must not exceed 100 characters.")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        [MaxLength(500, ErrorMessage = "Description must not exceed 500 characters.")]
+        public string? Description { get; set; }
+
+        [Url(ErrorMessage = "Invalid image URL.")]
+        public string? ImageUrl { get; set; }
+
+        public int PointsRequired { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Stock quantity cannot be negative.")]
+        public int StockQuantity { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public virtual ICollection<RewardRedemptions> ?RewardRedemptions { get; set; }
+    }
+}
