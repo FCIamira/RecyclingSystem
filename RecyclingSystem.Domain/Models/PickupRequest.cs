d9202b31a4d1fb9110967ba8dea6c6a1932e9a01
Your Name <you@example.com>
2025-06-30 05:41:01 +0200

    add DB

--- RecyclingSystem.Domain/Models/PickupRequest.cs
+++ RecyclingSystem.Domain/Models/PickupRequest.cs
@@ -3,9 +3,31 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RecyclingSystem.Domain.Models
-{
-    public class PickupRequest : BaseModel<int>
     {
+        public class PickupRequest : BaseModel<int>
+        {
+            [ForeignKey("User")]
+            public int CustomerId { get; set; }
 
+            public DateTime RequestedDate { get; set; }
+
+            public DateTime? ScheduledDate { get; set; }
+
+            public string Location { get; set; }
+
+            public string Note { get; set; }
+
+            public string Status { get; set; }
+
+            public int? EmployeeId { get; set; }
+
+            public DateTime? DateCollected { get; set; }
+
+            public int TotalPointsGiven { get; set; }
+
+
+            //Navigation Property
+            public virtual Users? User { get; set; }
+            public ICollection<PickupItem>? PickupItems { get; set; }
+        }
     }
-}
