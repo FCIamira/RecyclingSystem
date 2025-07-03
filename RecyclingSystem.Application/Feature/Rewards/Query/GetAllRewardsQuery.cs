64c0cac7f81d942efe3552f9ace3413a23bb1060
madonna-hany-boils <mh2153@fayoum.edu.eg>
2025-07-03 11:21:31 +0300

    update rewards controller

--- RecyclingSystem.Application/Feature/Rewards/Query/GetAllRewardsQuery.cs
+++ RecyclingSystem.Application/Feature/Rewards/Query/GetAllRewardsQuery.cs
@@ -1,28 +1,29 @@
-﻿//using MediatR;
-//using RecyclingSystem.Domain.Interfaces;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using MediatR;
+using RecyclingSystem.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace RecyclingSystem.Application.Feature.Rewards.Query
-//{
-//    public class GetAllRewardsQuery:IRequest
-//    {
-//    }
+namespace RecyclingSystem.Application.Feature.Rewards.Query
+{
+    public class GetAllRewardsQuery : IRequest
+    {
+    }
 
-//    public class GetAllRewardsQueryHandler:IRequestHandler<GetAllRewardsQuery>
-//    {
-//        private readonly IUnitOfWork unitOfWork;
-//        public GetAllRewardsQueryHandler( IUnitOfWork unitOfWork) { 
-//        this.unitOfWork = unitOfWork;
-        
-//        }
+    public class GetAllRewardsQueryHandler : IRequestHandler<GetAllRewardsQuery>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public GetAllRewardsQueryHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
 
-//        public Task Handle(GetAllRewardsQuery request, CancellationToken cancellationToken)
-//        {
-//          return  unitOfWork.rewards.GetAll();
-//        }
-//    }
-//}
+        }
+
+        public Task Handle(GetAllRewardsQuery request, CancellationToken cancellationToken)
+        {
+            return unitOfWork.rewards.GetAll();
+        }
+    }
+}
