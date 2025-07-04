3e04025b705e7b1eee1f65a67ec9483f7e50825f
MarlyMonsef <marlymonsef21@gmail.com>
2025-07-04 21:47:49 +0300

    update user

---
+++ RecyclingSystem.Application/Feature/UserInfo/Queries/GetUserQuery.cs
@@ -0,0 +1,30 @@
+﻿//using MediatR;
+//using RecyclingSystem.Application.Behaviors;
+//using RecyclingSystem.Application.DTOs.UserInfDTOs;
+//using RecyclingSystem.Domain.Interfaces;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace RecyclingSystem.Application.Feature.UserInfo.Queries
+//{
+//    public class GetUserQuery : IRequest<Result<UserInfoDto>>
+//    {
+//    }
+
+//    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, Result<UserInfoDto>>
+//    {
+//        private readonly IUnitOfWork _unitOfWork;
+//        public GetUserQueryHandler(IUnitOfWork unitOfWork)
+//        {
+//            _unitOfWork = unitOfWork;
+//        }
+
+//        public Task<Result<UserInfoDto>> Handle(GetUserQuery request, CancellationToken cancellationToken)
+//        {
+            
+//        }
+//    }
+//}
