f6577f6a3382944714c9f28d575e2443020c40f1
MarlyMonsef <marlymonsef21@gmail.com>
2025-07-04 14:27:45 +0300

    pass

---
+++ RecyclingSystem.API/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RecyclingSystem.Application.Behaviors;
+using RecyclingSystem.Application.Feature.UserInfo.Command;
+using RecyclingSystem.Domain.Enums;
+
+namespace RecyclingSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IMediator _mediator;
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpPost("CheckEmail")]
+        public async Task<IActionResult> checkEmail([FromForm] CheckEmailCommand command)
+        {
+            var exist = await _mediator.Send(command);
+            return Ok(exist);
+        }
+
+        [HttpPost("ChangePassword")]
+        [Authorize]
+        public async Task<IActionResult> ChangePassword([FromForm] ChangePasswordCommand command)
+        {
+            var message = await _mediator.Send(command);
+            return Ok(new { message });
+        }
+    }
+}
---
+++ RecyclingSystem.Application/Feature/UserInfo/Command/CheckEmailCommand.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using RecyclingSystem.Application.Behaviors;
+using RecyclingSystem.Domain.Enums;
+using RecyclingSystem.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecyclingSystem.Application.Feature.UserInfo.Command
+{
+
+    public class CheckEmailCommand : IRequest<Result<ApplicationUser>>
+    {
+        public string Email { get; set; }
+    }
+
+    public class CheckEmailCommandHandler : IRequestHandler<CheckEmailCommand, Result<ApplicationUser>>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<CheckEmailCommandHandler> _logger;
+        public CheckEmailCommandHandler(UserManager<ApplicationUser> userManager, ILogger<CheckEmailCommandHandler> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public async Task<Result<ApplicationUser>> Handle(CheckEmailCommand request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Checking if email exists.");
+            try
+            {
+                var user = await _userManager.FindByEmailAsync(request.Email);
+                if (user == null)
+                {
+                    _logger.LogWarning("Email not found.");
+                    return Result<ApplicationUser>.Failure(ErrorCode.NotFound, "Email not found.");
+                }
+                return Result<ApplicationUser>.Success(user);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while checking the email.");
+                return Result<ApplicationUser>.Failure(ErrorCode.ServerError, "An unexpected error occurred while processing your request.");
+            }
+        }
+    }
+}
