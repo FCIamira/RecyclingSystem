8754d924aea5c5da19260ba8d907c6a699baa646
madonna-hany-boils <mh2153@fayoum.edu.eg>
2025-07-14 23:44:17 +0300

    add warehouse

---
+++ RecyclingSystem.API/Controllers/WarehouseInventoryController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecyclingSystem.Application.Feature.Warehouses.Commonds;
+using RecyclingSystem.Application.Feature.WarehousingInventory.Commands;
+
+namespace RecyclingSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehouseInventoryController : ControllerBase
+    {
+       private readonly IMediator mediator;
+        public WarehouseInventoryController( IMediator mediator) { 
+         this.mediator = mediator;
+        }
+
+       [ HttpPost]
+ public async Task<IActionResult> addWarehouseInventory( int materialId,int quantity)
+        {
+
+            var query=new AddWharehosingInventoryCommand { MaterialId = materialId,Quantity = quantity };
+            var result=await mediator.Send(query);
+            if(result.IsSuccess) 
+            return Ok(result);
+
+            return BadRequest(result);
+
+        }
+    }
+}
