ad55c50af585d43823f0cd28f499f3cea07b5a5f
MarlyMonsef <marlymonsef21@gmail.com>
2025-07-15 05:14:27 +0300

    points

--- RecyclingSystem.API/Controllers/PointsHistoryController.cs
+++ RecyclingSystem.API/Controllers/PointsHistoryController.cs
@@ -17,6 +17,7 @@ namespace RecyclingSystem.API.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> GetAllUserHistory()
         {
             var history = await _mediator.Send(new GetAllPointsHistoryQuery());
