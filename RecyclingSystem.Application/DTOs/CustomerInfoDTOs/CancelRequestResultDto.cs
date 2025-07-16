9d35a4651efb1f15981cb89a668b709a90fc3554
Your Name <you@example.com>
2025-07-16 16:25:16 +0200

    add Cancel For Customer

---
+++ RecyclingSystem.Application/DTOs/CustomerInfoDTOs/CancelRequestResultDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecyclingSystem.Application.DTOs.CustomerInfoDTOs
+{
+    public class CancelRequestResultDto
+    {
+        public string Message { get; set; } = string.Empty;
+        public bool ShouldDeductPoints { get; set; }
+    }
+
+}
